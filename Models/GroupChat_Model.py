dc178ebded8375ad5637354fdcf609da8d4bd07d
trannhatnam2707 <tran.nam27072003@gmail.com>
2026-03-17 18:41:48 +0700

    complete Auth Middleware

--- Models/GroupChat_Model.py
+++ Models/GroupChat_Model.py
@@ -1,8 +1,8 @@
-from pydantic import Field
+from pydantic import BaseModel, Field
 
 
-class GroupChat_Model:
+class GroupChat_Model(BaseModel):
     group_id: str = Field(...,description="Mã nhóm chat ")
     group_name: str = Field(..., description= "Tên nhóm chat")
     owner_id: str = Field(..., description="Tham chiếu tới _id của bảng Users (Chủ nhóm)")
-    member: str = Field(...,description="các thành viên trong nhóm")
+    member: list[str] = Field(...,description="các thành viên trong nhóm")
